d506d0ef9a8ac40f91a1d38cdca8f931464414f1
damirovas <sdamirova2004@gmail.com>
2024-02-06 18:30:47 +0400

    .....

---
+++ ternaryOperator/src/js/app.js
@@ -0,0 +1,13 @@
+let student = true
+let message = student?'youre a student':"youre not student";
+console.log(message);
+
+
+
+let time =19
+let greeting = time<=12? "sabahinn xeir":"axsamin xeir";
+console.log(greeting);
+
+let purchase = 1250
+let discount = purchase>=1000 ?'your a rich':"youra poor"
+console.log(discount);
