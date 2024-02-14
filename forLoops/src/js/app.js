7f14681b7355148301ae00b28087b407d21be40f
damirovas <sdamirova2004@gmail.com>
2024-02-15 02:23:06 +0400

    ....

---
+++ forLoops/src/js/app.js
@@ -0,0 +1,21 @@
+
+  // let row = 1;
+  // let stars = 1;
+  // height=5
+
+  // while (row <= height) {
+  //     console.log(' '.repeat(height - row) + '*'.repeat(stars));
+  //     stars += 2;
+  //     row++;
+  // }
+
+
+
+  for(let i=1;i<=20;i++){
+    if(i %2 !==0){
+      continue
+    }else{
+      console.log(i);
+    }
+
+  }
