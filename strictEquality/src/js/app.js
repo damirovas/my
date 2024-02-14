90e4cdb523b4195fdf194d4bb9d38827095cf680
damirovas <sdamirova2004@gmail.com>
2024-02-15 01:37:59 +0400

    .....

---
+++ strictEquality/src/js/app.js
@@ -0,0 +1,14 @@
+const Pi = 3.14;
+if(Pi == "3.14"){
+  console.log("this is a pi")
+}else(
+  console.log("this is a not pi")
+)
+
+
+const e = 2.71;
+if(Pi === "2.71"){
+  console.log("this is a e")
+}else(
+  console.log("this is a not e")
+)
---
+++ whileLoops/src/js/app.js
@@ -0,0 +1,41 @@
+// let loggedIn = false;
+// let userName;
+// let password ;
+
+
+
+// while(!loggedIn){
+// userName= window.prompt("enter your username");
+// password= window.prompt("enter your password");
+// if(userName==="myUsername" && password==="myPassword"){
+// loggedIn=true;
+// console.log("your logged in");
+
+// }else(
+//   console.log("invalid credentitons")
+// )
+// }
+
+
+
+// let sayi = 1;
+
+// while (sayi <= 100) {
+//   if (sayi % 2 ===0) {
+//     console.log(sayi);
+//   }
+//   sayi++;
+// }
+
+
+let number = 1;
+
+while(number<= 50){
+  
+  if(number%2 ===0){
+console.log(number);
+  }
+  number++;
+}
+
+
